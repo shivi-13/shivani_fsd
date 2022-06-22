3553e4c447ca8be4e7aa746f6e0ba1216c9c84f0
Shivani Soni <sonishivani505@gmail.com>
2022-06-22 09:50:20 +0530

    Adding assignment

---
+++ A21-06/check.js
@@ -0,0 +1,11 @@
+function check(num)
+{
+    if (num%2==0)
+    console.log(num + "is even");
+
+    else
+    console.log(num + "is odd");
+
+}
+
+check(20);
---
+++ A21-06/converting.js
@@ -0,0 +1,4 @@
+var km=10;
+var miles;
+miles=km*0.621;
+console.log(km + "km in miles is"+ miles +"miles");
